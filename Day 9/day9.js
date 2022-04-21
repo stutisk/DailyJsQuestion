7b88654891da18550281af17b76dc4ab84631751
stutisk <48529714+stutisk@users.noreply.github.com>
2022-04-21 18:56:36 +0530

    question 1

--- Day 9/day9.js
+++ Day 9/day9.js
@@ -1,6 +1,24 @@
 // given three angles of triangle, check whether its scalene , isosceles , equilateral or not a triangle
 
 // question 1
-const typeOfTriangle = () => {
-    
+const typeOfTriangle = (a, b, c) => {
+
+  if(a===b && b===c){
+      console.log("equilateral traingle")
+  }
+  else if (a===b || b===c || c===a){
+      console.log("isosceles triangle")
+  }
+  else if(a!=b && b!=c && c!=a){
+    console.log("scalene traingle")
+  }
+  else{
+    console.log("not a triangle")
+  }
 };
+
+typeOfTriangle(30,60,90);
+
+// question 2
+
+// given two dates function should return which one comes first
